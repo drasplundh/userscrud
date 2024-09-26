65dddcaf3a505721e4ae64dcf5d34ba19a7ae734
Duncan Asplundh <drasplundh@gmail.com>
2024-09-26 15:27:58 -0400

    creating repository

---
+++ src/main/java/com/example/userscrud/service/PostServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.userscrud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.userscrud.entity.Post;
+import com.example.userscrud.repository.PostRepository;
+import com.example.userscrud.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class PostServiceImpl implements PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	
+	@Override
+	public Post createPost(Post post) {
+		return postRepository.save(post);
+	}
+
+}
